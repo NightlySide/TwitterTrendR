1d14b43ea388293941df0858736a3ce62fb6c6d3
nightlyside <nightlyside@gmail.com>
2021-02-01 12:21:33 +0100

    Switching to typescript

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,16 +1,14 @@
-<script>
-	export let name;
-
-	const fetchTrends = async() => {
+<script lang="ts">
+	const fetchTrends: Promise<any[]> = (async() => {
 		const results = await fetch("https://twittertrendr.herokuapp.com/trends");
 		return results.json();
-	}
+	})();
 </script>
 
 <main>
 	<h1>Hello!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{#await fetchTrends()}
+	{#await fetchTrends}
 		<p>Loading...</p>
 	{:then trends}
 		{#each trends as trend}
@@ -20,25 +18,3 @@
 		{error}
 	{/await}
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
